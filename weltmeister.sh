40bd43c41f030e136e7f968ca6e95f1d2cb66d93
jeremy <jeremy@rixon.org>
2016-07-21 20:23:53 +1000

    Fixed weltmeister.sh

---
+++ weltmeister.sh
@@ -0,0 +1,4 @@
+BEECON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo bee-con dir is $BEECON_DIR
+( sleep 2; open http://localhost:8080/weltmeister.html ) &
+php -S localhost:8080 -t "$BEECON_DIR/src/main/webapp"
