bf1e0b551bb398fec0e424cb4a056814138e0bc0
jeremy <jeremy@rixon.org>
2016-07-21 19:32:21 +1000

    Mavenise

---
+++ env.sh
@@ -0,0 +1,6 @@
+BEECON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo bee-con dir is $BEECON_DIR
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+echo JAVA_HOME=$JAVA_HOME
+export PATH=$BEECON_DIR/apache-maven-3.3.9/bin:$PATH
+echo mvn is $BEECON_DIR/apache-maven-3.3.9/bin/mvn
