11b05b603da9a9cea19685b157e785a896086c59
jeremy <jeremy@rixon.org>
2016-07-24 21:01:12 +1000

    ...

---
+++ src/main/webapp/lib/game/entities/combee.js
@@ -0,0 +1,19 @@
+ig.module(
+    'game.entities.combee'
+)
+.requires(
+    'impact.entity'
+)
+.defines(function() {
+	EntityCombee = ig.Entity.extend({
+		animSheet: new ig.AnimationSheet( 'media/combee_front_sheet.gif', 96, 48 ),
+		size:  {x: 96, y: 48},
+		offset: {x: 0, y: 0},
+		flip: false,
+		init: function( x, y, settings ) {
+			this.parent( x, y, settings );
+			this.addAnim( 'idle',  .05, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42] );
+			this.currentAnim = this.anims.idle;
+		},
+	})
+});
--- src/main/webapp/lib/game/main.js
+++ src/main/webapp/lib/game/main.js
@@ -3,40 +3,39 @@ ig.module(
 )
 .requires(
 	'impact.game',
-	'impact.font'
+	'impact.font',
+	'game.levels.chocobohouse'
 )
 .defines(function(){
 
 MyGame = ig.Game.extend({
 	
-	// Load a font
 	font: new ig.Font( 'media/04b03.font.png' ),
 	
-	
 	init: function() {
+		this.loadLevel( LevelChocobohouse );
 	},
 	
 	update: function() {
-		// Update all entities and backgroundMaps
 		this.parent();
-		
-		// Add your own, additional update code here
+		var combee = this.getEntitiesByType( EntityCombee )[0];
+		if (combee) {
+			
+		}
 	},
 	
 	draw: function() {
-		// Draw all entities and backgroundMaps
 		this.parent();
-		
-		
 		// Add your own drawing code here
 		var x = ig.system.width/2,
 			y = ig.system.height/2;
 		
-		this.font.draw( 'It Works! ' + ig.system.width + ' ' + ig.ua.viewport.width, x, y, ig.Font.ALIGN.CENTER );
+		this.font.draw( 'It Works! ' + ' ' + scale, x, y, ig.Font.ALIGN.CENTER );
 	}
 });
 
-var scale = ig.ua.pixelRatio * 2;
-ig.main( '#canvas', MyGame, 60, ig.ua.viewport.width / scale, ig.ua.viewport.height / scale, scale);
+var scale = 2; // ig.ua.pixelRatio * 2;
+
+ig.main( '#canvas', MyGame, 40, ig.ua.viewport.width / scale, ig.ua.viewport.height / scale, scale);
 
 });
